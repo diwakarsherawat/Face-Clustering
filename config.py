a6de766fd20df3e2971b00df020e835d3cb7ab44
diwakarsherawat <sherawat02813@gmail.com>
2025-06-23 19:20:51 +0530

    config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+# config.py
+
+# Input & output folders
+IMAGE_FOLDER = "/content/drive/MyDrive/neww"
+OUTPUT_FOLDER = "/content/drive/MyDrive/So"
+INVALID_FOLDER = "/content/drive/MyDrive/Invalid_Faces"
+
+# Clustering
+PCA_VARIANCE = 0.90
+K_CLUSTERS = 6  # Update after elbow method
